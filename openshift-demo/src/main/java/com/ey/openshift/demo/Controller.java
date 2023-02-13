3ce54570f700a315fe696952b945a1a3f272a38c
Sayan Samajdar <sayan.samajdar@gds.ey.com>
2023-02-13 16:01:26 +0530

    spring boot war

---
+++ openshift-demo/src/main/java/com/ey/openshift/demo/Controller.java
@@ -0,0 +1,18 @@
+package com.ey.openshift.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	@GetMapping("/hello")
+    String return1(){
+        return "Hello World";
+    }
+	
+	@GetMapping("/")
+    String return2(){
+        return "Welcome!!";
+    }
+}
+ 
